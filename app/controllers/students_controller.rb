63c34d61744786aefe21d352ff19a9f44e591b40
Luiz Cezer <lccezinha@gmail.com>
2014-12-04 21:27:42 -0200

    WIP: Students#index Students#new

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,17 @@
 class StudentsController < ApplicationController
+
+  # FIXME
+  def index
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :register_number)
+  end
+
 end
--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 RSpec.describe StudentsController, :type => :controller do
 
+  context 'GET #index' do
+    it 'render index template' do
+      get :index
+      expect(response).to render_template('index')
+    end
+
+    xit 'show list of students'
+  end
+
+  context 'GET #new' do
+    it 'render new template' do
+      get :new
+      expect(response).to render_template('new')
+    end
+
+    it 'assign new student instance' do
+      get :new
+      expect(assigns(:student)).to be_a_new(Student)
+    end
+  end
+
 end
