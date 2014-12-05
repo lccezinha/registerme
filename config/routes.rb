53419cdf590042094d7d8eba58c3822e81e7542f
Luiz Cezer <lccezinha@gmail.com>
2014-12-04 22:41:50 -0200

    WIP: home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :students
+  get  'home/index' => 'home#index'
 
-  root 'students#index'
+  root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
