d5d856ffd2f0ba68fb02e817b88bb1b9cffc81d2
Luiz Cezer <lccezinha@gmail.com>
2014-12-05 00:15:18 -0200

    Specs Controller + melhoria nas views

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,14 @@
 class StudentsController < ApplicationController
+  before_action :load_student, only: [:edit, :show, :update, :destroy]
 
   def index
     @students = Student.all
+  end
+
+  def show
+  end
+
+  def edit
   end
 
   def new
@@ -15,14 +22,30 @@ class StudentsController < ApplicationController
       flash[:notice] = t('controllers.students.flash.create.notice')
       redirect_to students_path
     else
-      render :new
+      render action: :new
     end
   end
 
-  # def edit
-  # end
+  def update
+    if @student.update(student_params)
+      flash[:notice] = t('controllers.students.flash.edit.notice')
+      redirect_to student_path(@student)
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    @student.destroy
+    flash[:notice] = t('controllers.students.flash.destroy.notice')
+    redirect_to students_path
+  end
 
   private
+
+  def load_student
+    @student = Student.find params[:id]
+  end
 
   def student_params
     params.require(:student).permit(:name, :register_number)
--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -18,6 +18,20 @@ RSpec.describe StudentsController, :type => :controller do
     end
   end
 
+  context 'GET #show' do
+    subject(:student) { create(:student) }
+
+    it 'load student' do
+      get :show, id: student.id
+      expect(assigns(:student)).to eq(student)
+    end
+
+    it 'render show template' do
+      get :show, id: student.id
+      expect(response).to render_template('show')
+    end
+  end
+
   context 'GET #new' do
     it 'render new template' do
       get :new
@@ -31,7 +45,6 @@ RSpec.describe StudentsController, :type => :controller do
   end
 
   context 'POST #create' do
-
     context 'when success' do
       subject(:student) { attributes_for(:student) }
 
@@ -56,7 +69,68 @@ RSpec.describe StudentsController, :type => :controller do
         expect(response).to render_template('new')
       end
     end
+  end
 
+  context 'GET #edit' do
+    subject(:student) { create(:student) }
+
+    it 'load student to edit' do
+      get :edit, id: student.id
+      expect(assigns(:student)).to eq(student)
+    end
+
+    it 'render edit template' do
+      get :edit, id: student.id
+      expect(response).to render_template('edit')
+    end
+  end
+
+  context 'PUT #update' do
+    context 'when success' do
+      subject(:student) { create(:student) }
+
+      it 'update student' do
+        put :update, id: student.id, student: attributes_for(:student)
+        expect(assigns(:student)).to eq(student)
+      end
+
+      it 'change information' do
+        put :update, id: student.id, student: { name: 'Cezer Marrone' }
+        expect(student.reload.name).to eq('Cezer Marrone')
+      end
+
+      it 'redirect to show' do
+        put :update, id: student.id, student: attributes_for(:student)
+        expect(response).to redirect_to(student_path(student))
+      end
+    end
+
+    context 'when fail' do
+      subject(:student) { create(:student, name: 'Luiz Cezer') }
+
+      it 'do not change information' do
+        put :update, id: student.id, student: { name: nil }
+        expect(student.reload.name).to eq('Luiz Cezer')
+      end
+
+      it 'render edit template' do
+        put :update, id: student.id, student: { name: nil }
+        expect(response).to render_template('edit')
+      end
+    end
+  end
+
+  context 'DELETE #destroy' do
+    subject(:student) { create(:student) }
+
+    it 'delete' do
+      expect{ delete :destroy, { id: student } }.to change(Student, :count).by(-1)
+    end
+
+    it 'render index' do
+      delete :destroy, id: student.id
+      expect(response).to render_template('index')
+    end
   end
 
 end
